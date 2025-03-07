b2a23b7db61404561c3944e0dc452077eb7b7ed0
zzzzzuuuuu <agu0830@naver.com>
2025-03-07 13:38:56 +0900

    refactor: 버튼 공통 컴포넌트 타입 전역적으로 한번에 관리하도록 수정

--- src/shared/types/button.types.ts
+++ src/shared/types/button.types.ts
@@ -1,6 +1,29 @@
+import {
+  buttonSizes,
+  verticalButtonSizes,
+  buttonStyles,
+} from '@/shared/components/button/button.constants.ts';
+
 export interface IButtonStyle {
   backgroundColor: string;
   textColor: string;
   hoverBackgroundColor?: string;
   borderColor?: string;
+}
+
+export interface IButtonProps {
+  size: keyof typeof buttonSizes;
+  style: keyof typeof buttonStyles;
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+}
+
+export interface IVerticalButtonProps {
+  size: keyof typeof verticalButtonSizes;
+  icon: React.ReactNode;
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
 }
