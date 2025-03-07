abda1fbe04b57c990011e97e421f27886b20cf8c
zzzzzuuuuu <agu0830@naver.com>
2025-03-07 22:57:56 +0900

    feat: 메모 작성 UI 구현

---
+++ src/shared/hooks/useTags.ts
@@ -0,0 +1,112 @@
+import { KeyboardEvent, useEffect, useState } from 'react';
+import { TeamTag } from '@/shared/types/tag.types.ts';
+
+interface TagsProps {
+  initialTags?: TeamTag[];
+  onEditTeamTag?: (tagId: number, newName: string) => void;
+  onCreateRoleTag?: (name: string) => void;
+  onEditRoleTag?: (tagId: number, newName: string) => void;
+  onDeleteRoleTag?: (tagId: number) => void;
+}
+
+export const useTags = ({
+  initialTags = [],
+  onEditTeamTag,
+  onCreateRoleTag,
+  onEditRoleTag,
+  onDeleteRoleTag,
+}: TagsProps) => {
+  const [tags, setTags] = useState<TeamTag[]>(initialTags); // 태그 업데이트
+  const [showTagInput, setShowTagInput] = useState<boolean>(false); // 태그 입력 인풋창 보여줄지
+  const [newTag, setNewTag] = useState<string>(''); // 새로운 태그 입력값
+  const [editTagIndex, setEditTagIndex] = useState<number | null>(null); // 태그 수정시 인덱스값
+
+  const handleAddTag = async (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && newTag.trim() !== '') {
+      // 태그 중복 체크
+      const isDuplicated = tags.some((tag) => tag.name === newTag.trim());
+      if (isDuplicated) {
+        alert('이미 존재하는 태그입니다!');
+        return;
+      }
+
+      if (onCreateRoleTag) {
+        await onCreateRoleTag(newTag.trim());
+      }
+      setTags([...tags, { tagId: Date.now(), name: newTag.trim() }]);
+      setNewTag('');
+      setShowTagInput(false);
+    }
+  };
+
+  const handleEditTag = async (
+    e: KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    if (e.key === 'Enter' && newTag.trim() !== '') {
+      // 태그 중복 체크
+      const isDuplicated = tags.some((tag) => tag.name === newTag.trim());
+      if (isDuplicated) {
+        alert('이미 존재하는 태그입니다!');
+        return;
+      }
+      const updatedTags = [...tags];
+      const tagId = updatedTags[index].tagId; // 기존 태그의 ID를 유지
+      updatedTags[index] = { tagId, name: newTag.trim() };
+
+      setTags(updatedTags);
+      setEditTagIndex(null);
+      setNewTag('');
+      setShowTagInput(false);
+
+      // 팀 태그와 역할 태그에 따라 다른 콜백 호출
+      if (tagId !== undefined) {
+        if (onEditTeamTag) {
+          await onEditTeamTag(tagId, newTag.trim());
+        } else if (onEditRoleTag) {
+          await onEditRoleTag(tagId, newTag.trim());
+        }
+      }
+    }
+  };
+
+  const startEditingTag = (index: number) => {
+    setEditTagIndex(index);
+    setNewTag(tags[index].name);
+    setShowTagInput(true);
+  };
+
+  const handleDeleteTag = async (index: number) => {
+    const tagId = tags[index]?.tagId;
+    if (tagId !== undefined && onDeleteRoleTag) {
+      await onDeleteRoleTag(tagId);
+    }
+    setTags((prevTags) => {
+      const updatedTags = prevTags.filter((_, i) => i !== index);
+      setEditTagIndex(null);
+      setNewTag('');
+      return updatedTags;
+    });
+  };
+
+  useEffect(() => {
+    if (tags.length < 3) {
+      setShowTagInput(false);
+    }
+  }, [tags]);
+
+  return {
+    tags,
+    showTagInput,
+    newTag,
+    editTagIndex,
+    handleAddTag,
+    handleEditTag,
+    startEditingTag,
+    handleDeleteTag,
+    setTags,
+    setShowTagInput,
+    setEditTagIndex,
+    setNewTag,
+  };
+};
