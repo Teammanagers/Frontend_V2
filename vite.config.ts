7ec913f8975829cdf668ea3a492858f1b3ad677e
zzzzzuuuuu <agu0830@naver.com>
2025-03-07 22:50:25 +0900

    chore: svgr 설치 및 vite.config.ts 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@/app', replacement: '/src/app' },
