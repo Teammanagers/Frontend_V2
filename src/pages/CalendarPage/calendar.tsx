2dd448d57c1f5fc08aaa02c6d25a44eaf23547b7
김예안 <kjan0929@naver.com>
2025-03-25 21:27:40 +0900

    style: 캘린더 페이지 레이아웃 및 다가오는 일정 UI 구현 [TM-113]

--- src/pages/CalendarPage/calendar.tsx
+++ src/pages/CalendarPage/calendar.tsx
@@ -1,3 +1,45 @@
+import { UpcomingSchedule } from '@/entities/schedule/ui';
+import Calendar from '@/widgets/calendar/Calendar';
+import styled from 'styled-components';
+
 export function CalendarPage() {
-  return <div>Calendar</div>;
+  return (
+    <Container>
+      <Calendar />
+
+      <UpcomingSchedulesWrapper>
+        <UpcomingScheduleTitle>다가오는 일정</UpcomingScheduleTitle>
+        <UpcomingScheduleList>
+          {Array.from({ length: 3 }).map((_, idx) => (
+            <UpcomingSchedule key={idx} />
+          ))}
+        </UpcomingScheduleList>
+      </UpcomingSchedulesWrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 99px 0 0 112px;
+`;
+
+const UpcomingSchedulesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 436px;
+`;
+
+const UpcomingScheduleTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const UpcomingScheduleList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
