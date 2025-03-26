a77611be339cca1fe0948cc98d064d0d34a96408
kojesung <js95112345@gmail.com>
2025-03-27 02:07:46 +0900

    feat : Input 공통 컴포넌트 파일 생성[TM-123]

---
+++ src/shared/components/input/Input.tsx
@@ -0,0 +1,4 @@
+export default function Input() {
+  // 제목, placeholder, content일 수도 있고, children 컴포넌트가 들어갈 수도 있음, valid option(유효성 검사나 action을 했을 때 밑에 띄워주거나 하는 등의 next option)
+  return <div></div>;
+}
