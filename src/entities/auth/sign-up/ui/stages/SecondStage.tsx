005fd471ee7e38ca469f68e5c06d91c36db93779
kojesung <js95112345@gmail.com>
2025-03-27 21:43:51 +0900

    feat : 회원가입/로그인 완료 부분 UI[TM-123]

--- src/entities/auth/sign-up/ui/stages/SecondStage.tsx
+++ src/entities/auth/sign-up/ui/stages/SecondStage.tsx
@@ -1,21 +1,54 @@
-import Input from '@/shared/components/input/Input';
+import styled from 'styled-components';
+import signUpImg from '@/shared/assets/common/signupImg.svg?url';
+import { Button } from '@/shared/components/button/Button';
 
 export default function SecondStage() {
   return (
-    <div>
-      <Input
-        title="Team code"
-        subTitle="어쩌구저쩌구"
-        placeholder="팀명 또는 프로젝트명을 입력해 주세요"
-      />
-      <Input
-        title="아이이잇"
-        subTitle="ㅋㅋㅋ"
-        placeholder="어어업"
-        textColor="blue"
-        showHelperMessage={true}
-        helperMessage="아이요"
-      />
-    </div>
+    <Wrapper>
+      <img src={signUpImg} width={364} height={244} />
+      <Message>환영합니다!</Message>
+      <BtnContainer>
+        <BtnText>이제 팀매니저를 사용할 수 있어요!</BtnText>
+        <Button size="large" style="main">
+          우리 팀 만들러 가기
+        </Button>
+        <BtnText>이미 다른 팀의 초대를 받았나요?</BtnText>
+        <Button size="large" style="sub">
+          팀 참가하러 가기
+        </Button>
+      </BtnContainer>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 547px;
+`;
+
+const Message = styled.span`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 150%;
+  color: rgba(29, 29, 29, 1);
+`;
+
+const BtnContainer = styled.div`
+  margin-top: 18px;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-items: center;
+`;
+
+const BtnText = styled.span`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 150%;
+  color: rgba(29, 29, 29, 1);
+`;
