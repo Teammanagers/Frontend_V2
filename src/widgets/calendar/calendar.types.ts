8edb634a2342e968c8c1ffba559b054da83ef01a
김예안 <kjan0929@naver.com>
2025-03-28 16:11:20 +0900

    style: 캘린더 UI 및 기본 동작 구현 [TM-127]

--- src/widgets/calendar/calendar.types.ts
+++ src/widgets/calendar/calendar.types.ts
@@ -1,5 +1,8 @@
 import { IModal } from '@/shared/types';
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 type EventEditorMode = 'register' | 'edit' | 'read';
 
 interface IEventEditorModalProps extends Omit<IModal, 'children'> {
@@ -7,4 +10,4 @@ interface IEventEditorModalProps extends Omit<IModal, 'children'> {
   setModalMode: React.Dispatch<React.SetStateAction<EventEditorMode>>;
 }
 
-export type { EventEditorMode, IEventEditorModalProps };
+export type { Value, EventEditorMode, IEventEditorModalProps };
