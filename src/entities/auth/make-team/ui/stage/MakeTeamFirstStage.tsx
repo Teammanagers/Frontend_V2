42da3a83dfe1ae654271132bc1bf99f1e066df76
kojesung <js95112345@gmail.com>
2025-04-02 18:05:01 +0900

    style : 팀 생성 레이아웃[TM-123]

--- src/entities/auth/make-team/ui/stage/MakeTeamFirstStage.tsx
+++ src/entities/auth/make-team/ui/stage/MakeTeamFirstStage.tsx
@@ -1,18 +1,22 @@
 import styled from 'styled-components';
+import camera from '@/shared/assets/common/cam-plus.svg?url';
+import arrow from '@/shared/assets/common/expand-right-arrow.svg?url';
 import { Button } from '@/shared/components/button/Button';
 import Input from '@/shared/components/input/Input';
 
 export default function MakeTeamFirstStage() {
   return (
     <MakeTeamWrapper>
+      <BackContainer>
+        <img src={arrow} width={40} height={40} />
+        <BackSpan>프로젝트를 위해 팀을 생성해주세요</BackSpan>
+      </BackContainer>
       <TopContainer>
         <ImgContainer>
-          <img />
+          <img src={camera} width={163} height={163} />
         </ImgContainer>
-        <span></span>
       </TopContainer>
-      <div></div>
-      <div>
+      <BottomContainer>
         <Input
           title="Title"
           placeholder="팀명 또는 프로젝트명을 입력해 주세요"
@@ -23,7 +27,7 @@ export default function MakeTeamFirstStage() {
         <Button size="xxl" style="main">
           팀 생성 완료
         </Button>
-      </div>
+      </BottomContainer>
     </MakeTeamWrapper>
   );
 }
@@ -38,6 +42,33 @@ const MakeTeamWrapper = styled.div`
   align-items: center;
 `;
 
+const BackContainer = styled.div`
+  position: absolute;
+  top: 100px;
+  left: 112px;
+  display: flex;
+`;
+
+const BackSpan = styled.span`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 150%;
+  color: black;
+`;
+
 const TopContainer = styled.div``;
 
-const ImgContainer = styled.div``;
+const BottomContainer = styled.div`
+  margin-top: 17px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+
+const ImgContainer = styled.div`
+  width: 163px;
+  height: 163px;
+  border-radius: 38px;
+  border: solid 1px rgba(240, 240, 240, 1);
+  background-color: white;
+`;
