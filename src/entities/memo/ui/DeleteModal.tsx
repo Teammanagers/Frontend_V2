cfba75383dbd6bb4f0a51dcd4356cc40d843ce27
zzzzzuuuuu <agu0830@naver.com>
2025-04-28 22:39:37 +0900

    refactor: 삭제 모달 온오프 로직 분리

--- src/entities/memo/ui/DeleteModal.tsx
+++ src/entities/memo/ui/DeleteModal.tsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
-import { DeleteModalProps } from '@/entities/memo/memo.type.ts';
-import { Button } from '@/shared/components/button/Button.tsx';
+import { DeleteModalProps } from '@/entities/memo/memo.type';
+import { Button } from '@/shared/components/button/Button';
+import Modal from '@/shared/components/modal/Modal.tsx';
 
 export const DeleteModal = ({ type, name, onClose }: DeleteModalProps) => {
   const handleDelete = () => {
     if (type === 'folder') {
       console.log('폴더 삭제 로직');
-    } else if (type === 'memo') {
+    } else {
       console.log('메모 삭제 로직');
     }
   };
+
   return (
-    <Overlay onClick={onClose}>
-      <ModalContainer onClick={(e) => e.stopPropagation()}>
+    <Modal isOpen={true} toggle={onClose}>
+      <ModalContainer>
         {`'${name}' ${type === 'folder' ? '폴더' : '메모'}를 삭제하시겠습니까?`}
         <ButtonContainer>
           <Button size="mini" style="main" onClick={onClose}>
@@ -23,22 +25,9 @@ export const DeleteModal = ({ type, name, onClose }: DeleteModalProps) => {
           </Button>
         </ButtonContainer>
       </ModalContainer>
-    </Overlay>
+    </Modal>
   );
 };
-
-export const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.1);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
 
 const ModalContainer = styled.div`
   display: flex;
