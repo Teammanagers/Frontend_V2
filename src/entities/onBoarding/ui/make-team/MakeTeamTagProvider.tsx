37a2c6ae70065abf902643e0909fa7f5b5068652
kojesung <js95112345@gmail.com>
2025-07-24 20:20:43 +0900

    feat: Tag 관련 Context 생성

---
+++ src/entities/onBoarding/ui/make-team/MakeTeamTagProvider.tsx
@@ -0,0 +1,71 @@
+import { createContext, ReactNode, useContext, useEffect, useRef } from 'react';
+import { useTags } from '@/shared/hooks/useTags';
+import { TeamTag } from '@/shared/types/tag.types.ts';
+
+interface MakeTeamTagContextType {
+  tags: TeamTag[];
+  showTagInput: boolean;
+  setShowTagInput: (show: boolean) => void;
+  newTag: string;
+  setNewTag: (tag: string) => void;
+  handleAddTag: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  handleRMTag: (index: number) => void;
+  inputRef: React.RefObject<HTMLInputElement>;
+}
+
+const MakeTeamTagContext = createContext<MakeTeamTagContextType | null>(null);
+
+interface MakeTeamTagProviderProps {
+  children: ReactNode;
+}
+
+export default function MakeTeamTagProvider({
+  children,
+}: MakeTeamTagProviderProps) {
+  const {
+    tags,
+    setTags,
+    showTagInput,
+    setShowTagInput,
+    newTag,
+    setNewTag,
+    handleAddTag,
+  } = useTags({ initialTags: [] });
+
+  useEffect(() => {
+    if (showTagInput) {
+      inputRef?.current?.focus();
+    }
+  }, [showTagInput]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleRMTag = (index: number) => {
+    setTags(tags.filter((_, idx) => idx !== index));
+  };
+
+  return (
+    <MakeTeamTagContext.Provider
+      value={{
+        tags,
+        showTagInput,
+        setShowTagInput,
+        newTag,
+        setNewTag,
+        handleAddTag,
+        handleRMTag,
+        inputRef,
+      }}
+    >
+      {children}
+    </MakeTeamTagContext.Provider>
+  );
+}
+
+export function useTagContext() {
+  const context = useContext(MakeTeamTagContext);
+  if (!context) {
+    throw new Error('useTagContext must be used within TagProvider');
+  }
+  return context;
+}
