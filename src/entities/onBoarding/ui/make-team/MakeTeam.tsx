0ee56175941bc30d5c01746395660eea14463f92
kojesung <js95112345@gmail.com>
2025-07-24 20:22:36 +0900

    feat: 팀 생성 컴포넌트 Tag Provider로 래핑

--- src/entities/onBoarding/ui/make-team/MakeTeam.tsx
+++ src/entities/onBoarding/ui/make-team/MakeTeam.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import MakeTeamTagProvider from './MakeTeamTagProvider';
 import MakeTeamFirstStage from './stage/MakeTeamFirstStage';
 import MakeTeamSecondStage from './stage/MakeTeamSecondStage';
 
 export default function MakeTeam() {
   const [stage, setStage] = useState<number>(1);
-  if (stage === 1) {
-    return <MakeTeamFirstStage setStage={setStage}></MakeTeamFirstStage>;
-  } else {
-    return <MakeTeamSecondStage></MakeTeamSecondStage>;
-  }
+
+  return (
+    <MakeTeamTagProvider>
+      {stage === 1 && <MakeTeamFirstStage setStage={setStage} />}
+      {stage === 2 && <MakeTeamSecondStage />}
+    </MakeTeamTagProvider>
+  );
 }
