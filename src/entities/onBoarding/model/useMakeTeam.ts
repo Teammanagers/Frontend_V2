6ee7b38fda254f10b7736bccb2252f0d6080a49f
kojesung <js95112345@gmail.com>
2025-07-29 00:10:26 +0900

    feat: file upload 훅 생성

--- src/entities/onBoarding/model/useMakeTeam.ts
+++ src/entities/onBoarding/model/useMakeTeam.ts
@@ -1,4 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import apiRequest from '@/shared/api/apiRequest';
 import { TeamTag } from '@/shared/types/tag.types';
 
@@ -8,6 +9,26 @@ interface createTeamMutationProps {
   teamTagList: TeamTag[];
   password: string;
 }
+
+export const useTeamImgUpload = () => {
+  const [postImg, setPostImg] = useState<File | null>(null);
+  const [previewImg, setPreviewImg] = useState<string | null>(null);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setPostImg(file);
+
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        setPreviewImg(e.target?.result as string);
+      };
+      fileReader.readAsDataURL(file);
+    }
+  };
+
+  return { postImg, setPostImg, previewImg, setPreviewImg, handleFileUpload };
+};
 
 export const useCreateTeam = () => {
   const createTeamMutation = useMutation({
