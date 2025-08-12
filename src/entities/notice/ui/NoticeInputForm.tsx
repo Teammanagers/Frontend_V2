90efe901478592436d033c83359bf27c8904bb46
김예안 <kjan0929@naver.com>
2025-08-12 15:22:42 +0900

    refactor: 공지, 피드백 폼 제출 버튼 공용화 [TM-119]

--- src/entities/notice/ui/NoticeInputForm.tsx
+++ src/entities/notice/ui/NoticeInputForm.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import NoticeSubmitBtn from '@/shared/assets/main/notice-submit-btn.svg?react';
 import { useState } from 'react';
 import useBulletPointConverter from '@/shared/hooks/action/useBulletPointConverter';
 import useNoticeQueries from '../model/useNoticeQueries';
+import FormSubmitButton from '@/shared/components/button/FormSubmitButton';
 
 function NoticeInputForm({ toggle }: { toggle: () => void }) {
   const [inputValue, setInputValue] = useState<string>('');
@@ -34,9 +34,8 @@ function NoticeInputForm({ toggle }: { toggle: () => void }) {
         onChange={handleChange}
         onKeyDown={(e) => useBulletPointConverter({ e, setInputValue })}
       />
-      <Button type="submit">
-        <NoticeSubmitBtn />
-      </Button>
+
+      <FormSubmitButton type="submit" />
     </FormContainer>
   );
 }
@@ -66,24 +65,5 @@ const Textarea = styled.textarea`
     font-size: 12px;
     font-weight: 400;
     color: ${({ theme }) => theme.colors.darkGray};
-  }
-`;
-
-const Button = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 48px;
-  height: 36px;
-  border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.mainBlue};
-  font-size: 16px;
-  font-weight: 700;
-  color: ${({ theme }) => theme.colors.white};
-
-  &:hover {
-    background-color: #3c8bff;
-    transition: background-color 0.3s;
-    box-shadow: 0 1px 4px 0 rgba(60, 139, 255, 0.06);
   }
 `;
--- src/pages/ResourcePage/resource.tsx
+++ src/pages/ResourcePage/resource.tsx
@@ -1,3 +1,14 @@
+import AddFeedbackButton from '@/entities/feedback/AddFeedbackButton';
+import AddResourceButton from '@/entities/resource/AddResourceButton';
+
 export function ResourcePage() {
-  return <div>Share</div>;
+  return (
+    <div>
+      <div style={{ width: '486px', height: '66px' }}>
+        <AddResourceButton />
+      </div>
+      <AddResourceButton size="small" />
+      <AddFeedbackButton />
+    </div>
+  );
 }
