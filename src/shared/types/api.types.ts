5d91c8e892a5fda4f6d717e37d86362f0cb3d90b
김예안 <kjan0929@naver.com>
2025-10-02 21:39:27 +0900

    feat: 공통 APIResponse 타입 추가 [TM-75]

--- src/shared/types/api.types.ts
+++ src/shared/types/api.types.ts
@@ -1,5 +1,12 @@
 type APIMethodType = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
+interface APIResponse<T> {
+  isSuccess: boolean; // 성공 여부
+  code: string; // 상태 코드
+  message: string; // 응답 메시지
+  result: T; // 데이터 내용
+}
+
 interface IAPIRequestConfig {
   url: string;
   method?: APIMethodType;
@@ -13,4 +20,4 @@ type QueryResponse = {
   isError?: boolean;
 };
 
-export type { IAPIRequestConfig, QueryResponse };
+export type { IAPIRequestConfig, APIResponse, QueryResponse };
