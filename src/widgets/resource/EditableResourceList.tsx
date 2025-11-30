b4c9b63c097f5e72e1ddb9156e49da811a03c0ce
김예안 <kjan0929@naver.com>
2025-11-30 18:51:38 +0900

    feat: 자료공유 데이터 패칭 상태에 따른 로직 추가 [TM-77]

--- src/widgets/resource/EditableResourceList.tsx
+++ src/widgets/resource/EditableResourceList.tsx
@@ -7,6 +7,7 @@ import AddFeedbackButton from '@/features/feedback/ui/AddFeedbackButton';
 import { useResourceDeletion } from '@/features/resource/model/useResourceDeletion';
 import DeleteResourceButton from '@/features/resource/ui/DeleteResourceButton';
 import DeleteResourceModal from '@/features/resource/ui/DeleteResourceModal';
+import Skeleton from '@/shared/components/skeleton/Skeleton';
 import ResourceAddController from './ResourceAddController';
 
 export default function EditableResourceList({
@@ -15,7 +16,12 @@ export default function EditableResourceList({
   /* 선택된 자료를 상위 컴포넌트로 전달하는 콜백 함수 */
   onSelectedResource: Dispatch<SetStateAction<Resource | null>>;
 }) {
-  const { data: resources } = useGetResourceList(); // 자료 목록 조회
+  const {
+    data: resources,
+    isPending,
+    isSuccess,
+    isError,
+  } = useGetResourceList(); // 자료 목록 조회
   // 자료 삭제 로직 훅
   const {
     isDeleteModalOpen,
@@ -33,37 +39,52 @@ export default function EditableResourceList({
   return (
     <>
       <Container>
-        {/* 자료 목록 */}
-        <ResourceList>
-          {resources &&
-            resources.map((resource) => (
-              <ResourceWrapper key={resource.dataId}>
-                <ResourceCard
-                  data={resource}
-                  deleteButton={
-                    <DeleteResourceButton
-                      creatorId={resource.fileInfo.createdBy}
-                      onClick={(e) => openDeleteModal(e, resource.dataId)}
-                    />
-                  }
-                />
+        {isPending &&
+          Array.from({ length: 4 }).map((_, index) => (
+            <Skeleton key={index} width={486} height={66} />
+          ))}
 
-                {/* 피드백 추가 버튼 */}
-                <AddFeedbackButton
-                  onClick={() => onSelectedResource(resource)}
-                />
-              </ResourceWrapper>
-            ))}
+        {isError && (
+          <ErrorMessage>자료를 불러오는 중 오류가 발생했습니다.</ErrorMessage>
+        )}
 
-          {/* 스크롤 타겟 - 업로드 성공 시 이곳으로 스크롤되는 더미 태그 */}
-          <ScrollTarget ref={scrollRef} />
-        </ResourceList>
+        {isSuccess && (
+          <>
+            {/* 자료 목록 */}
+            <ResourceList>
+              {resources.map((resource) => (
+                <ResourceWrapper key={resource.dataId}>
+                  <ResourceCard
+                    data={resource}
+                    deleteButton={
+                      <DeleteResourceButton
+                        creatorId={resource.fileInfo.createdBy}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openDeleteModal(e, resource.dataId);
+                        }}
+                      />
+                    }
+                  />
 
-        {/* 자료 추가 컨트롤 위젯 */}
-        <ResourceAddController
-          resourceCount={resources ? resources.length : 0}
-          onUploadSuccess={scrollToBottom}
-        />
+                  {/* 피드백 추가 버튼 */}
+                  <AddFeedbackButton
+                    onClick={() => onSelectedResource(resource)}
+                  />
+                </ResourceWrapper>
+              ))}
+
+              {/* 스크롤 타겟 - 업로드 성공 시 이곳으로 스크롤되는 더미 태그 */}
+              <ScrollTarget ref={scrollRef} />
+            </ResourceList>
+
+            {/* 자료 추가 컨트롤 위젯 */}
+            <ResourceAddController
+              resourceCount={resources ? resources.length : 0}
+              onUploadSuccess={scrollToBottom}
+            />
+          </>
+        )}
       </Container>
 
       <DeleteResourceModal
@@ -76,6 +97,7 @@ export default function EditableResourceList({
 }
 
 const Container = styled.section`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -103,3 +125,15 @@ const ResourceWrapper = styled.li`
 `;
 
 const ScrollTarget = styled.div``;
+
+const ErrorMessage = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.black};
+`;
