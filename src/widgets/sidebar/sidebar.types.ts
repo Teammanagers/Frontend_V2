9f04a10c1027f14938f49882b9b39d1a5ce1c402
zzzzzuuuuu <agu0830@naver.com>
2025-12-01 18:28:25 +0900

    fix: 불필요한 end 관련 타입 삭제

--- src/widgets/sidebar/sidebar.types.ts
+++ src/widgets/sidebar/sidebar.types.ts
@@ -8,11 +8,9 @@ export interface SideBarProps {
   expanded: boolean; // hover 등으로 펼침 여부
   activePath: string; // 현재 경로
   isAlarmOpen: boolean;
-  endSelected: boolean;
   team: TeamProps | null;
 
   onNavigate: (path: (teamId: number) => string) => void;
   onToggleAlarm: () => void;
   onToggleTeamList: () => void;
-  onEndClick: () => void;
 }
