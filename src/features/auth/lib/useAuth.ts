7989d719fb96989d976afc551043004b09ca59a9
김예안 <kjan0929@naver.com>
2025-12-11 11:45:50 +0900

    feat: 전역 상태를 활용하여 useAuth 훅 로그인, 로그아웃 로직 추가 [TM-89]

--- src/features/auth/lib/useAuth.ts
+++ src/features/auth/lib/useAuth.ts
@@ -1,4 +1,24 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE_SEGMENTS } from '@/app/routes/paths';
+import { useAuthStore } from '@/shared/model/store/authStore';
+import { useTeamStore } from '@/shared/model/store/teamStore';
+
 export const useAuth = () => {
-  const accessToken = localStorage.getItem('accessToken');
-  return !!accessToken;
+  const navigate = useNavigate();
+
+  const isAuth = useAuthStore((state) => state.isAuth);
+  const loginAction = useAuthStore((state) => state.login);
+  const logoutAction = useAuthStore((state) => state.logout);
+
+  const clearTeamIds = useTeamStore((state) => state.clearTeamIds);
+
+  const logout = useCallback(() => {
+    clearTeamIds();
+    logoutAction();
+
+    navigate(`/${ROUTE_SEGMENTS.LOGIN}`, { replace: true });
+  }, [clearTeamIds, logoutAction, navigate]);
+
+  return { isAuth, login: loginAction, logout };
 };
