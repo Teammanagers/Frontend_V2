351259d9096611ba31cd01b3a584c4f285e8db19
김예안 <kjan0929@naver.com>
2025-12-22 15:30:11 +0900

    refactor: 쿼리 키에 teamId 의존성 추가 [TM-164]
    팀 변경 시 이전 팀의 데이터가 캐시에 남아있는 현상 방지하기 위해 teamId를 쿼리 키에 추가
    (공지, 자료, 프로필, 다가오는 일정)

--- src/entities/user/model/useProfileQueries.ts
+++ src/entities/user/model/useProfileQueries.ts
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { User } from '@/entities/user/user.types';
 import apiRequest from '@/shared/api/apiRequest';
+import { useTeamStore } from '@/shared/model/store/teamStore';
 import { APIResponse } from '@/shared/types/api.types';
 
 // 프로필 조회
 export const useGetProfile = () => {
+  const teamId = useTeamStore((state) => state.teamId);
+
   return useQuery<APIResponse<User>, Error, User>({
-    queryKey: ['user', 'profile'],
+    queryKey: ['user', 'profile', teamId],
     queryFn: async () => {
       return await apiRequest({
         url: '/api/v2/member',
