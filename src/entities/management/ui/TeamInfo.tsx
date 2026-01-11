4206ba46cd5d37b94adc58b1a6b379f82d405ffd
zzzzzuuuuu <agu0830@naver.com>
2026-01-11 22:17:09 +0900

    fix: 팀 이미지 팀장만 수정할 수 있도록 변경

--- src/entities/management/ui/TeamInfo.tsx
+++ src/entities/management/ui/TeamInfo.tsx
@@ -80,9 +80,12 @@ export const TeamInfo = ({
 
   return (
     <Container>
-      <ProfileContainer onClick={handleImgClick}>
+      <ProfileContainer
+        onClick={isTeamLeader ? handleImgClick : undefined}
+        $disabled={!isTeamLeader}
+      >
         {profileImage ? <ProfileImg src={profileImage} /> : <DefaultImg />}
-        <UploadIconStyled />
+        {isTeamLeader && <UploadIconStyled />}
         <HiddenInput
           type="file"
           accept="image/jpeg, image/png"
@@ -149,11 +152,11 @@ const Container = styled.div`
   gap: 24px;
   width: 100%;
 `;
-const ProfileContainer = styled.div`
+const ProfileContainer = styled.div<{ $disabled: boolean }>`
   position: relative;
   width: 180px;
   height: 180px;
-  cursor: pointer;
+  cursor: ${({ $disabled }) => ($disabled ? 'default' : 'pointer')};
 `;
 
 const ProfileImg = styled.img`
