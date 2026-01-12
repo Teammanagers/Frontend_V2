40f4619c789711826d0c6c77c61a1043cdecee7d
김예안 <kjan0929@naver.com>
2026-01-12 13:08:13 +0900

    refactor: 전체 투두 반환 유틸함수 재사용가능하도록 분리 [TM-173]

--- src/entities/todo/lib/getProgressWidth.ts
+++ src/entities/todo/lib/getProgressWidth.ts
@@ -12,18 +12,20 @@ const calculateWidth = (total: number, count: number | undefined) => {
   return ((count ?? 0) / total) * 100;
 };
 
+const getTotalCount = (progressArr: ITeamProgressStatus[]) =>
+  progressArr.find((status) => status.label === '전체')?.count ?? 0;
+
 // 특정 라벨의 진행률을 계산하여 너비의 비율을 반환하는 함수
 const getProgressWidth = (
   label: string,
-  progressArr: ITeamProgressStatus[],
+  taemProgress: ITeamProgressStatus[],
 ) => {
   // 전체 count를 구함
-  const totalCount =
-    progressArr.find((status) => status.label === '전체')?.count ?? 0;
+  const totalCount = getTotalCount(taemProgress);
 
-  const count = getCountByLabel(label, progressArr); // 해당 label의 count를 가져옴
+  const count = getCountByLabel(label, taemProgress); // 해당 label의 count를 가져옴
 
-  return calculateWidth(totalCount, count);
+  return calculateWidth(totalCount, count) ?? 0;
 };
 
-export { getProgressWidth };
+export { getProgressWidth, getTotalCount };
