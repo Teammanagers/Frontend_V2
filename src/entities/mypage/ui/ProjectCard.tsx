054ea4fd9e7bb1c46942e86a4719888a3d004c0d
zzzzzuuuuu <agu0830@naver.com>
2026-01-19 23:50:15 +0900

    feat: 프로젝트 카드 UI 구현

---
+++ src/entities/mypage/ui/ProjectCard.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import ArrowIcon from '@/shared/assets/common/arrow.svg?react';
+
+interface ProjectCardProps {
+  id: number;
+  name: string;
+  duration: string;
+  selected: boolean;
+  onSelect: (id: number) => void;
+}
+
+export function ProjectCard({
+  id,
+  name,
+  duration,
+  selected,
+  onSelect,
+}: ProjectCardProps) {
+  return (
+    <CardContainer $selected={selected} onClick={() => onSelect(id)}>
+      <ProjectName>
+        <Name $selected={selected}>{name}</Name>
+        <Duration>{duration}</Duration>
+      </ProjectName>
+      <ArrowWrapper>
+        <ArrowIcon
+          width={24}
+          height={24}
+          stroke="1d1d1d"
+          strokeWidth={selected ? 2 : 1.5}
+        />
+      </ArrowWrapper>
+    </CardContainer>
+  );
+}
+
+const CardContainer = styled.div<{ $selected: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 487px;
+  height: 64px;
+  border-radius: 7px;
+  border: 1px solid
+    ${({ $selected, theme }) =>
+      $selected ? theme.colors.mainBlue : theme.colors.lightGray};
+  padding: 0 16px;
+
+  background: ${({ $selected, theme }) =>
+    $selected ? theme.colors.background : 'white'};
+`;
+
+const ProjectName = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Name = styled.p<{ $selected: boolean }>`
+  font-size: 16px;
+  font-weight: ${({ $selected }) => ($selected ? 700 : 500)};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const Duration = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+const ArrowWrapper = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
