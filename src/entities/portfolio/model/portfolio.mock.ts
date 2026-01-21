7a6ee88e3d6097e34a2c0209d0ec1ea1ab1a0e8f
zzzzzuuuuu <agu0830@naver.com>
2026-01-21 13:30:49 +0900

    chore: 프로젝트 목업데이터 분리

---
+++ src/entities/portfolio/model/portfolio.mock.ts
@@ -0,0 +1,30 @@
+export const MOCK_PROJECTS = [
+  {
+    id: 1,
+    name: '팀매니저 ver.1',
+    duration: '2024.07 ~ 2024.08',
+  },
+  {
+    id: 2,
+    name: '팀매니저 ver.2',
+    duration: '2024.09 ~ ing',
+  },
+];
+
+export const projectInfoMock = [
+  {
+    id: 1,
+    title: '프로젝트 카테고리',
+    tags: ['팀매니저', '팀프로젝트'],
+  },
+  {
+    id: 2,
+    title: '프로젝트 멤버',
+    tags: ['지유', '안지유'],
+  },
+  {
+    id: 3,
+    title: '나의 역할',
+    tags: ['개발자', '프론트엔드'],
+  },
+];
