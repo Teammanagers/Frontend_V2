d21413328c5f85cffa1b0c5c74643334c624a4ce
zzzzzuuuuu <agu0830@naver.com>
2026-01-23 22:06:47 +0900

    feat: 포트폴리오 스켈레톤 적용

--- src/pages/MyPage/portfolio.tsx
+++ src/pages/MyPage/portfolio.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {
   MOCK_PROJECTS,
   projectInfoMock,
 } from '@/entities/portfolio/model/portfolio.mock.ts';
+import Skeleton from '@/shared/components/skeleton/Skeleton.tsx';
 import MypageHeader from '@/widgets/mypage/MypageHeader.tsx';
 import { ProjectDetail } from '@/widgets/portfolio/ui/ProjectDetail.tsx';
 import { ProjectList } from '@/widgets/portfolio/ui/ProjectList.tsx';
@@ -13,6 +14,8 @@ export default function Portfolio() {
   const [selectedProjectId, setSelectedProjectId] = useState<number | null>(
     null,
   );
+  // TODO: API 연동 후 isPending으로 변경
+  const [isLoading, setIsLoading] = useState(true);
 
   // 프로젝트 존재 여부
   const hasProjects = projects.length > 0;
@@ -26,22 +29,38 @@ export default function Portfolio() {
     setSelectedProjectId((prev) => (prev === projectId ? null : projectId));
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Container>
       <MypageHeader showBackButton>포트폴리오</MypageHeader>
 
       <ContentWrapper>
-        <ProjectList
-          projects={projects}
-          hasProjects={hasProjects}
-          selectedProjectId={selectedProjectId}
-          onSelectProject={handleSelectProject}
-        />
-        <ProjectDetail
-          hasProjects={hasProjects}
-          selectedProject={selectedProject}
-          projectInfo={projectInfoMock}
-        />
+        {isLoading ? (
+          <>
+            <Skeleton width="534px" height="562px" />
+            <Skeleton width="534px" height="562px" />
+          </>
+        ) : (
+          <>
+            <ProjectList
+              projects={projects}
+              hasProjects={hasProjects}
+              selectedProjectId={selectedProjectId}
+              onSelectProject={handleSelectProject}
+            />
+            <ProjectDetail
+              hasProjects={hasProjects}
+              selectedProject={selectedProject}
+              projectInfo={projectInfoMock}
+            />
+          </>
+        )}
       </ContentWrapper>
     </Container>
   );
