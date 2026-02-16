904dc0f8060b55e685eeab707ab04317644878fd
김예안 <kjan0929@naver.com>
2026-02-16 17:17:12 +0900

    refactor: 토큰 갱신 인터셉터 예외 가드 추가 및 로직 개선 [TM-138]
    - error.config 미존재 시 런타임 에러 방지 가드 추가
    - refreshToken 누락 시 불필요한 API 요청 방지
    - 재요청시 Authorization 헤더 수동 주입 로직 제거 (request intercepter에 위임)

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -35,9 +35,11 @@ axiosInstance.interceptors.response.use(
   (response) => response,
 
   async (error) => {
+    if (!error.config) return Promise.reject(error);
+
     // 처음 서버에 보냈던 실패 요청 객체 정보를 가져와서 재시도하기 위해 에러 객체 꺼내옴
     // 토큰 갱신 후 여기에 새로운 accessToken을 덧붙여 다시 보내는 것!
-    const originalRequest: CustomInternalAxiosRequestConfig = error?.config;
+    const originalRequest: CustomInternalAxiosRequestConfig = error.config;
 
     // 401 에러이면서, 아직 재시도 하지 않은 요청일 때  => 아직 refresh를 시도하지 않은 경우
     if (
@@ -62,6 +64,10 @@ axiosInstance.interceptors.response.use(
       if (!refreshPromise) {
         refreshPromise = (async () => {
           const refreshToken = localStorage.getItem('refreshToken');
+
+          if (!refreshToken) {
+            throw new Error('사용 가능한 refreshToken이 없습니다.');
+          }
 
           const { data } = await axios.post(
             `${import.meta.env.VITE_BASE_URL}/api/v2/auth/reissue`,
@@ -90,8 +96,7 @@ axiosInstance.interceptors.response.use(
       // 이미 refresh 요청이 진행중이면 (401에러 발생 요청이 여러 개일 때)
       // 새로 refresh 요청을 보내지 않고, 기존 refreshPomise가 resolve될 때까지 대기
       // 모든 요청이 하나의 Promise를 공유하여 refresh 중복 제어
-      return refreshPromise.then((newAccessToken) => {
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+      return refreshPromise.then(() => {
         return axiosInstance(originalRequest); // 재갱신된 토큰으로 원본 요청 재시도
       });
     }
